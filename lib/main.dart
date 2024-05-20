9f15c5680038197d7cc599602c699bc731f88e2e
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-05-20 19:27:07 +0530

    Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,40 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String str = "Please Nannu Am Chayoddu";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'My First Flutter App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My App'),
+          title: Text('My First Flutter App'),
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:<Widget> [
-          Text("HI"),
-          Center(
-          child: TextButton(
-            child: Text('SHAIK ABDULHAMEED'),
-            onPressed: () {
-              print("Abbaa nokkesadu");
-            }, 
-          ),
-        ),],)
-        
+          children: [
+            Center(
+              child: Text("TESTING"),
+            ),
+            TextButton(
+              child: Text(str),
+              onPressed: () {
+                setState(() {
+                  str = "Nokkodu ra Nannu Am Chayoddu";
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
