8dab2a31e1c6279a9409dcf73eba004984092aea
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-05-28 14:41:03 +0530

    Plugins

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lecle_system_shortcuts/lecle_system_shortcuts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,17 +13,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('HEADER'),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Home"),
+              onPressed: () {
+                SystemShortcuts.home();
+              },
+            ),
+          ],
         ),
-        body: Center(
-          child:Image.asset("images/wall.jpg"),
-        ),
+        // body: Center(
+        //   child: Image.asset("images/wall.jpg"),
+        // ),
       ),
     );
   }
