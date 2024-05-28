55035d1921c2b9680566d4c3f3287e1f38a5908d
SHAIK ABDULHAMEED <shaikabdulhameedd@gmail.com>
2024-05-28 15:33:56 +0530

    Rows and Space Around

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lecle_system_shortcuts/lecle_system_shortcuts.dart';
 import 'package:lecle_system_shortcuts/lecle_system_shortcuts.dart';
 
 void main() {
@@ -17,20 +19,20 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('HEADER'),
-          actions: <Widget>[
-            TextButton(
-              child: Text("Home"),
-              onPressed: () {
-                SystemShortcuts.home();
-              },
-            ),
-          ],
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Image.network("https://th.bing.com/th/id/R.7e20cfd6ef87c40f71a85de25317f5ce?rik=PQ0DW0wMB3Nd1g&riu=http%3a%2f%2fanimalstime.com%2fwp-content%2fuploads%2f2016%2f02%2feastern-bluebird-2-1068x945.jpg&ehk=x1AEJYsn0NovjytI916a%2betvxuiHo5%2fepl7gZ%2bPYCLc%3d&risl=&pid=ImgRaw&r=0",height: 100,width: 300,),
+              Center(
+                child: Text(
+                  "Sample",
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+            ],
+          ),
         ),
-        // body: Center(
-        //   child: Image.asset("images/wall.jpg"),
-        // ),
       ),
     );
   }
